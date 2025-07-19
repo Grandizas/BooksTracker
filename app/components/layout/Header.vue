e19aaf39f2090780586f90e818da89f3cf7d42d9
Grazvydas Z <grazvydas.cherry@gmail.com>
2025-07-19 18:02:08 +0300

    New: First SCSS steps

---
+++ app/components/layout/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <header class="header">
+    <div class="header__left">Logo</div>
+    <div class="header__center">Navigation</div>
+    <div class="header__right">User Menu</div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@use "@/app/assets/style/layout/_header.scss";
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  modules: ["@nuxt/eslint"],
+  runtimeConfig: {
+    public: {
+      supabaseUrl: process.env.SUPABASE_URL,
+      supabaseKey: process.env.SUPABASE_ANON_KEY,
+    },
+  },
+  css: [
+    "@/assets/style/base/_base.scss",
+    "@/assets/style/base/_typography.scss",
+    "@/assets/style/base/_normalize.scss",
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@/assets/style/abstracts/variables" as *;
+            @use "@/assets/style/abstracts/functions" as *;
+          `,
+        },
+      },
+    },
+  },
+});
